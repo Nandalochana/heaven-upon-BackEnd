610181a6117937b9ead49618b2104c8344b07104
Mujitha Manorathna <mujitha.m3@gmail.com>
2025-09-27 14:20:51 +0300

    Revert "First Commit"
    This reverts commit b63bd5e54342ba9fda48c74b769d1ca29772b286.

--- src/api/navbar/destinations/route.js
+++
@@ -1,8 +0,0 @@
-import dbConnect from "../../../../utils/dbConnect.js";
-import Destination from "../../../../models/destination.js";
-
-export async function GET() {
-  await dbConnect();
-  const destinations = await Destination.find({});
-  return Response.json(destinations);
-}
--- src/models/destinationModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const DestinationSchema = new mongoose.Schema({
-  label: String,
-  link: String,
-});
-
-export default mongoose.models.Destination || mongoose.model("Destination", DestinationSchema);
